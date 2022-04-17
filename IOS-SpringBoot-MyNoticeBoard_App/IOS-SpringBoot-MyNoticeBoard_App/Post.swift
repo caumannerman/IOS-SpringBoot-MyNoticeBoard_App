f954efbc30de37fa0a626dc18cca37dae5a796bd
Caumannerman <uigohiiming2@gmail.com>
2022-04-18 01:55:25 +0900

    URLSession을 사용하여 SpringBoot서버 DB에서 Post 가져와 Json파싱 후 게시글 Plot

--- IOS-SpringBoot-MyNoticeBoard_App/IOS-SpringBoot-MyNoticeBoard_App/Post.swift
+++ IOS-SpringBoot-MyNoticeBoard_App/IOS-SpringBoot-MyNoticeBoard_App/Post.swift
@@ -11,7 +11,8 @@ struct Post: Codable{
     let id: Int?
     let title, content, userNickName: String?
     let time: String?
-    let hashTag: [String]?
+    //let hashTag: [String]?
+    let hashTag: String?
 
     enum CodingKeys: String, CodingKey {
         case id, title, content, time
--- IOS-SpringBoot-MyNoticeBoard_App/IOS-SpringBoot-MyNoticeBoard_App/Scenes/NoticeBoard/NoticeBoardViewController.swift
+++ IOS-SpringBoot-MyNoticeBoard_App/IOS-SpringBoot-MyNoticeBoard_App/Scenes/NoticeBoard/NoticeBoardViewController.swift
@@ -9,20 +9,22 @@ import UIKit
 import SnapKit
 
 class NoticeBoardViewController: UITableViewController {
-    private var posts: [Post] = [Post(id: 1, title: "제 첫 게시글 입니다.", content: "첫 번쨰 dummy게시글입니다.", userNickName: "nickname1", time: "12:31", hashTag: ["첫게시물","첫 게시","선팔"]),
-                                 Post(id: 2, title: "제 두번째 게시글 입니다.", content: "두 번쨰 dummy게시글입니다.", userNickName: "nickname2", time: "21:07", hashTag: ["두번째 게시물","첫 게시","선팔"]),
-                                 Post(id: 3, title: "제 3 게시글 입니다.", content: "세 번쨰 dummy게시글입니다.", userNickName: "nickname3", time: "15:14", hashTag: ["세 번째 게시물","3 게시","333","3"]),
-                                 Post(id: 4, title: "제 4 게시글 입니다.", content: "네 번쨰 dummy게시글입니다.", userNickName: "nickname4", time: "11:16", hashTag: ["네번째 게시물","4 게시","four"]),
-                                 Post(id: 5, title: "제 5 게시글 입니다.", content: "다섯 번쨰 dummy게시글입니다.", userNickName: "nickname5", time: "09:57", hashTag: ["dummy","미라","이집트","피라미드"]),
-                                 Post(id: 6, title: "제 6 게시글 입니다.", content: "여섯 번쨰 dummy게시글입니다.", userNickName: "nickname6", time: "03:22", hashTag: ["게시물","소통"]),
-                                 Post(id: 7, title: "제 7 게시글 입니다.", content: "일곱 번쨰 dummy게시글입니다.", userNickName: "nickname7", time: "19:36", hashTag: ["마지막 게시물","마지막","선팔","맞팔","소통"])]
-    
+//    private var posts: [Post] = [Post(id: 1, title: "제 첫 게시글 입니다.", content: "첫 번쨰 dummy게시글입니다.", userNickName: "nickname1", time: "12:31", hashTag: ["첫게시물","첫 게시","선팔"]),
+//                                 Post(id: 2, title: "제 두번째 게시글 입니다.", content: "두 번쨰 dummy게시글입니다.", userNickName: "nickname2", time: "21:07", hashTag: ["두번째 게시물","첫 게시","선팔"]),
+//                                 Post(id: 3, title: "제 3 게시글 입니다.", content: "세 번쨰 dummy게시글입니다.", userNickName: "nickname3", time: "15:14", hashTag: ["세 번째 게시물","3 게시","333","3"]),
+//                                 Post(id: 4, title: "제 4 게시글 입니다.", content: "네 번쨰 dummy게시글입니다.", userNickName: "nickname4", time: "11:16", hashTag: ["네번째 게시물","4 게시","four"]),
+//                                 Post(id: 5, title: "제 5 게시글 입니다.", content: "다섯 번쨰 dummy게시글입니다.", userNickName: "nickname5", time: "09:57", hashTag: ["dummy","미라","이집트","피라미드"]),
+//                                 Post(id: 6, title: "제 6 게시글 입니다.", content: "여섯 번쨰 dummy게시글입니다.", userNickName: "nickname6", time: "03:22", hashTag: ["게시물","소통"]),
+//                                 Post(id: 7, title: "제 7 게시글 입니다.", content: "일곱 번쨰 dummy게시글입니다.", userNickName: "nickname7", time: "19:36", hashTag: ["마지막 게시물","마지막","선팔","맞팔","소통"])]
+    var postList: [Post] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.register(NoticeBoardViewCell.self, forCellReuseIdentifier: "NoticeBoardViewCell")
         tableView.rowHeight = 100.0
+        
+        getAllPosts()
         
     }
     
@@ -31,21 +33,69 @@ class NoticeBoardViewController: UITableViewController {
 
 extension NoticeBoardViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts.count
+        return postList.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "NoticeBoardViewCell", for: indexPath) as? NoticeBoardViewCell else { return UITableViewCell()}
        
-        cell.setup(with: posts[indexPath.row])
+        cell.setup(with: postList[indexPath.row])
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let noticeBoardDetailView = NoticeBoardDetailViewController()
-        noticeBoardDetailView.post = posts[indexPath.row]
+        noticeBoardDetailView.post = postList[indexPath.row]
         //self.show(noticeBoardDetailView, sender: nil)
         self.navigationController?.pushViewController(noticeBoardDetailView, animated: true)
+    }
+}
+
+
+extension NoticeBoardViewController{
+    func getAllPosts() {
+        guard let url = URL(string: "http://localhost:8080/api/posts/") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            guard error == nil,
+                  let self = self,
+                  let response = response as? HTTPURLResponse,
+                  let data = data,
+                  let posts = try? JSONDecoder().decode([Post].self, from : data) else {
+                print("ERROR: URLSession data task \(error?.localizedDescription ?? "")")
+                return
+            }
+            
+            switch response.statusCode{
+            case(200...299)://성공
+                self.postList += posts
+                
+                //UI작업은 main스레드에서 하도록
+                DispatchQueue.main.async{
+                    self.tableView.reloadData()
+                }
+            case(400...499)://클라이언트 에러
+                print("""
+ERROR: Client ERROR \(response.statusCode)
+Response: \(response)
+""")
+            case(500...599)://서버에러
+                print("""
+ERROR: Server ERROR \(response.statusCode)
+Response: \(response)
+""")
+            default://이외
+                print("""
+ERROR: ERROR \(response.statusCode)
+Response: \(response)
+""")
+                
+            }
+        }
+        dataTask.resume() // 해당 task를 실행
     }
 }
