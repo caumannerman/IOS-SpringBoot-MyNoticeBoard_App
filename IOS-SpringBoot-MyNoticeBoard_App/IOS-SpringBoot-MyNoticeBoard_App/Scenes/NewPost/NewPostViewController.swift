0deee490795dbd2f995ce100adbb1e05b3544fb6
Caumannerman <uigohiiming2@gmail.com>
2022-04-18 15:24:39 +0900

    게시글 작성 ViewController에서 URLSession을 이용해 POST메서드로 DB에 게시글 저장

--- IOS-SpringBoot-MyNoticeBoard_App/IOS-SpringBoot-MyNoticeBoard_App/Scenes/NewPost/NewPostViewController.swift
+++ IOS-SpringBoot-MyNoticeBoard_App/IOS-SpringBoot-MyNoticeBoard_App/Scenes/NewPost/NewPostViewController.swift
@@ -46,13 +46,27 @@ class NewPostViewController: UIViewController {
         return textView
     }()
     
+    private lazy var hashtagTextField: UITextField = {
+        let textField = UITextField()
+        textField.font = .systemFont(ofSize: 17.0, weight: .bold)
+        textField.textColor = .black
+        
+        
+        textField.layer.borderWidth = 2.5
+        textField.layer.cornerRadius = 10.0
+        textField.layer.borderColor = UIColor(red: 70/255, green: 205/255, blue: 170/255, alpha: 1.0).cgColor
+        
+        textField.placeholder = "해시태그를 입력하세요."
+       
+        return textField
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         switch postEditMode {
         case .new:
-            self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "등록", style: .plain, target: self, action: nil)
+            self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "등록", style: .plain, target: self , action: #selector(createPost))
             self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "임시저장", style: .plain, target: self, action: nil)
         case .edit(_):
             self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "수정", style: .plain, target: self, action: nil)
@@ -63,6 +77,80 @@ class NewPostViewController: UIViewController {
         configureEditMode()
         
     }
+                                                                     
+    @objc func createPost(){
+        print("pressed")
+        guard let url = URL(string: "http://localhost:8080/api/posts/") else {
+            print("ERROR: Cannot create URL")
+            return
+        }
+        
+        let newPost: Post = Post(id: nil, title: titleTextField.text, content: contentsTextView.text, userNickName: "new닉네임s", time: "13:31", hashTag: hashtagTextField.text)
+
+        guard let jsonData = try? JSONEncoder().encode(newPost) else {
+            print("ERROR: Trying to convert model to JSON data")
+            return
+        }
+        
+        print("mymy" )
+        print(jsonData)
+        print("mymy End")
+        
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        //body에 담아 보낸다
+        request.httpBody = jsonData
+        
+        let dataTask = URLSession.shared.dataTask(with: request) {  data, response, error in
+            guard error == nil else{
+                print("ERROR: error calling POST")
+                return
+            }
+            guard let response = response as? HTTPURLResponse else{
+                print("ERROR: Http request failed")
+                return
+            }
+            guard let data = data else{
+                print("ERROR: Did not receive data")
+                return
+            }
+            guard let posts = try? JSONDecoder().decode(Post.self, from : data) else {
+                print("ERROR: URLSession data task \(error?.localizedDescription ?? "")")
+                return
+            }
+
+            switch response.statusCode{
+            case(200...299)://성공
+                print("post 성공")
+                print(posts)
+
+                //UI작업은 main스레드에서 하도록
+//                DispatchQueue.main.async{
+//                    self.tableView.reloadData()
+//                }
+            case(400...499)://클라이언트 에러
+                print("""
+ERROR: Client ERROR \(response.statusCode)
+Response: \(response)
+""")
+            case(500...599)://서버에러
+                print("""
+ERROR: Server ERROR \(response.statusCode)
+Response: \(response)
+""")
+            default://이외
+                print("""
+ERROR: ERROR \(response.statusCode)
+Response: \(response)
+""")
+
+            }
+        }
+    dataTask.resume() // 해당 task를 실행
+            }
     
     private func placeholderSetting(){
         contentsTextView.delegate = self
@@ -77,21 +165,19 @@ class NewPostViewController: UIViewController {
             self.titleTextField.text = post.title
             self.contentsTextView.text = post.content
             self.contentsTextView.textColor = .black
+            self.hashtagTextField.text = post.hashTag
+           
         default:
             break
-        
         }
-           
         }
-    
 }
 
 private extension NewPostViewController{
     func setupSubview(){
-        [titleTextField, contentsTextView].forEach{
+        [titleTextField, contentsTextView, hashtagTextField].forEach{
             view.addSubview($0)
         }
-        
         
         titleTextField.snp.makeConstraints{
             $0.top.equalTo(view.safeAreaLayoutGuide).offset(10.0)
@@ -103,6 +189,13 @@ private extension NewPostViewController{
             $0.leading.trailing.equalToSuperview().inset(12.0)
             $0.top.equalTo(titleTextField.snp.bottom).offset(10)
             $0.height.equalTo(300)
+        }
+        
+        hashtagTextField.snp.makeConstraints{
+            $0.leading.trailing.equalToSuperview().inset(12.0)
+            $0.top.equalTo(contentsTextView.snp.bottom).offset(10)
+            $0.height.equalTo(35)
+            
         }
     }
 }
@@ -120,5 +213,6 @@ extension NewPostViewController: UITextViewDelegate{
             contentsTextView.text = "내용을 입력해주세요."
             contentsTextView.textColor = .lightGray
         }
+        
     }
 }
